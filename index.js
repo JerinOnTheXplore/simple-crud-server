a81263984ab6d04e9981635cd3054bf85a50a75c
JerinXplore <jerinjerin101325@gmail.com>
2025-05-14 19:57:47 +0600

    Updated user to database

--- index.js
+++ index.js
@@ -46,6 +46,25 @@ async function run() {
         const newUser = req.body;
        const result = await  usersCollection.insertOne(newUser);
        res.send(result);
+    });
+
+    app.put('/users/:id',async(req,res)=>{
+        const id =req.params.id;
+        const filter ={_id: new ObjectId(id)};
+        const user = req.body;
+
+        const updatedDoc ={
+            $set:{
+                name:user.name,
+                email:user.email,
+            }
+        }
+        const options = { upsert: true };
+        console.log(user);
+
+        const result=await usersCollection.updateOne(filter,updatedDoc,options);
+        res.send(result);
+        
     })
 
     app.delete('/users/:id',async(req,res)=>{
